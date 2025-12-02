2a5ec6b0d3cc892af6a378b747e06562a4494fa1
ChristianHirschey <christian.hirschey@gmail.com>
2025-12-02 02:01:31 +0000

    add day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::ifstream inputFile("day1.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    int currNum = 50;
+    int zeroCount = 0;
+
+    while (inputFile >> line) {
+        if (line[0] == 'L') {
+            currNum -= std::stoi(&line[1]);
+        } else {
+            currNum += std::stoi(&line[1]);
+        }
+        if (currNum % 100 == 0) {
+            zeroCount++;
+        }
+    }
+
+    inputFile.close();
+
+    std::cout << "Total times at zero: " << zeroCount << std::endl;
+
+    return zeroCount;
+}
